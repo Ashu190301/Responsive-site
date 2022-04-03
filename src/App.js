90e1565da7a9215d80fb5e4c814981d16b0dadd3
Ashu190301 <ashu190301@gmail.com>
2022-04-04 00:01:41 +0530

    full-site

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages";
-import { BrowserRouter as Router } from "react-router-dom";
+import SigninPage from "./pages/signin";
 
 function App() {
   return (
     <Router>
-      <Home />
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/signin" component={SigninPage} />
+      </Switch>
     </Router>
   );
 }
