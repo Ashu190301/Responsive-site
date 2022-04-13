4ed0f31dfeb2fdcbf834864a79d78981893d523f
Ashu190301 <ashu190301@gmail.com>
2022-04-14 01:08:58 +0530

    signin

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ScrollToTop from '../components/ScrollToTop'
-import SignIn from '../Signin'
+import React from "react";
+import ScrollToTop from "../components/ScrollToTop";
+import SignIn from "../components/Signin";
 
 const SigninPage = () => {
   return (
     <>
-    <ScrollToTop />
+      <ScrollToTop />
       <SignIn />
     </>
-  )
-}
+  );
+};
 
-export default SigninPage
+export default SigninPage;
